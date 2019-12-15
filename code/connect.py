c977af8a1bc836d0f508c9b79454c23167e5e01f
TylersDurden <srobbin1@ithaca.edu>
2019-12-14 21:19:11 -0500

    minor tweaks

--- code/connect.py
+++ code/connect.py
@@ -78,7 +78,8 @@ def serve():
             else:       # KNOWN PEER
                 print '[*] Known Peer %s connecting' % client_addr[0]
                 client_key = client.recv(1024)
-                cipher = AES.new(client_key)
+                print '[*] Received Client Key: %s' % base64.b64encode(client_key)
+                cipher = AES.new(base64.b64decode(client_key))
                 local_key = security.load_key()
                 client.send(base64.b64encode(local_key))
                 client.close()
