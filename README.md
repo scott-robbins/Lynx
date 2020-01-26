da71557daf69fb3a083e668b64641df3c8e5a216
TylersDurden <srobbin1@ithaca.edu>
2020-01-26 01:17:01 -0500

    adding public pages

---
+++ README.md
@@ -0,0 +1,6 @@
+# LYNX 
+A simple peer to peer file sharing and messaging application. 
+
+This project is currently under development 
+
+**Last Updated January 2020**
--- code_py2.7/frontend/serve.py
+++ code_py2.7/frontend/serve.py
@@ -80,6 +80,7 @@ try:
             # os.remove('info.html')
         elif 'GET /favicon.ico HTTP/1.1' in request.split('\r\n'):
             user_agent = ''
+            print '[*] Displaying Info'
             client.send('HTTP 200 OK\r\n'+open(html_engine.display_information(client_addr[0], user_agent), 'rb').read())
             os.remove('info.html')
             time.sleep(0.1)
