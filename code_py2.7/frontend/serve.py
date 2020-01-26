71a5adae00d6073358d3613855e9903f7b7f0fdc
TylersDurden <srobbin1@ithaca.edu>
2020-01-26 13:32:55 -0500

    working on peer registration

--- code_py2.7/frontend/serve.py
+++ code_py2.7/frontend/serve.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
     print '[*] %d Registered Users ' % len(users.keys())
 
     # Start listener daemon for new user credential uploads
-    os.system('$(python engine.py -l %d)&>&1 ' % runtime)
+    os.system('"$(python engine.py -l %d)&" >&1 ' % runtime)
 
     # Start HTTP Server
     tic = time.time()
