4e8dc8021012bbe0b5d636e19595ab9db620e24b
TylersDurden <srobbin1@ithaca.edu>
2020-01-26 01:01:48 -0500

    adding public pages

--- code_py2.7/frontend/serve.py
+++ code_py2.7/frontend/serve.py
@@ -79,6 +79,7 @@ try:
             client.send('HTTP 200 OK\r\n'+open(html_engine.display_information(client_addr[0], user_agent), 'rb').read())
             # os.remove('info.html')
         elif 'GET /favicon.ico HTTP/1.1' in request.split('\r\n'):
+            user_agent = ''
             client.send(open(html_engine.display_information(client_addr[0], user_agent), 'rb').read())
             os.remove('info.html')
         else:
