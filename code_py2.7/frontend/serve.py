4b6e28776d8d9c22566d760a4ff2e2879bb7681f
TylersDurden <srobbin1@ithaca.edu>
2020-02-03 17:54:46 -0500

    trying to fix some error handling edge case

--- code_py2.7/frontend/serve.py
+++ code_py2.7/frontend/serve.py
@@ -65,7 +65,7 @@ def run(handler):
                 request = client.recv(2048)
             except socket.error:
                 print '[*] %s disconnected unexpectedly' % client_addr[0]
-                pass
+                continue
             registered_users = refresh_users()
             user_agent = ''
             for ln in request.split('\r\n'):
