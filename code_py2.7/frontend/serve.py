0f3bed0129334fd07445f7ee79a1376a18b338af
TylersDurden <srobbin1@ithaca.edu>
2020-02-08 22:42:13 -0500

    last commit broke server, but I used file sharing code to transfer back the last commit that did work, so thats cool!

--- code_py2.7/frontend/serve.py
+++ code_py2.7/frontend/serve.py
@@ -49,80 +49,16 @@ def refresh_users():
     return unames
 
 
-class HttpServer:
-    tic = 0
-
-    def __init__(self):
-        self.tic = time.time()
-        self.requests = {'GET / HTTP/1.1': self.show_login,
-                         'GET /assets/img/logo.png HTTP/1.1': self.display_logo,
-                         'GET /favicon.ico HTTP/1.1': time.sleep(0.1),
-                         'POST / HTTP/1.1': self.submit,
-                         'GET /FAQ HTTP/1.1': self.show_faq}
-
-    @staticmethod
-    def get_user_agent(request):
-        user_agent = ''
-        for ln in request.split('\r\n'):
-            if 'User-Agent:' in ln.split(' '):
-                try:
-                    user_agent = ln.split('User-Agent:')[1].replace('\n', '')
-                except IndexError:
-                    pass
-        return user_agent
-
-    def show_login(self, c):
-        status = True
-        try:
-            c.send(open('login.html', 'rb').read())
-        except socket.error:
-            status = False
-        return c, status
-
-    def display_logo(self, c):
-        status = True
-        try:
-            c.send(open('assets/img/logo.png', 'rb').read())
-        except socket.error:
-            status = False
-            pass
-        try:
-            os.remove('info.html')
-        except OSError:
-            status = False
-            pass
-        return c, status
-
-    def submit(self, c):
-        state = True
-        msg = '[*] %s is submitting login information.\n' % c.getpeername()
-        try:
-            open(log_file_name, 'a').write(msg)
-        except OSError:
-            state = False
-        return c, state
-
-    def show_faq(self, c):
-        status = True
-        try:
-            c.send(open('assets/faq.html', 'rb').read())
-        except socket.error:
-            status = False
-        return c, status
-
 def run(handler):
-    server = HttpServer()
     clients = []
     active_clients = {}
     running = True
     # Accept incoming requests
     try:
-        while running and (time.time() - server.tic) < runtime:
+        while running and (time.time() - tic) < runtime:
             client, client_addr = handler.accept()
             if client_addr[0] not in list(set(clients)):
-                d, l = create_timestamp()
                 new_client = True
-                open(log_file_name,'a').write('[*] %s has connected [%s]\n' % (client_addr[0], l))
             else:
                 new_client = False
             try:
@@ -131,36 +67,50 @@ def run(handler):
                 print '[*] %s disconnected unexpectedly' % client_addr[0]
                 continue
             registered_users = refresh_users()
+            user_agent = ''
+            for ln in request.split('\r\n'):
+                if 'User-Agent:' in ln.split(' '):
+                    try:
+                        user_agent = ln.split('User-Agent:')[1].replace('\n', '')
+                    except IndexError:
+                        pass
 
-            user_agent = server.get_user_agent(request)
-
-            query = request.split('\r\n')
-
-            # Handle all requests with http server
-            if query in server.requests.keys():
-                client, status = server.requests[query](client)
-
-            # Requests that I havent figured out with the server class
-            if 'GET /inbox HTTP/1.1' in query and not new_client:
+            # Serve login page to new connections, and handle logins
+            if 'GET / HTTP/1.1' in request.split('\r\n'):
+                client.send(open('login.html', 'rb').read())
+            # Display logo
+            elif 'GET /assets/img/logo.png HTTP/1.1' in request.split('\r\n'):
+                client.send(open('assets/img/logo.png', 'rb').read())
+                try:
+                    os.remove('info.html')
+                except OSError:
+                    continue
+            # Display information about downloading the client
+            elif 'GET /favicon.ico HTTP/1.1' in request.split('\r\n'):
+                time.sleep(0.1)
+            elif 'POST / HTTP/1.1' in request.split('\r\n'):
+                open(log_file_name, 'a').write('[*] %s is submitting login information.\nUser Agent: %s\n' %
+                                               (client_addr[0], user_agent))
+            elif 'GET /inbox HTTP/1.1' in request.split('\r\n') and not new_client:
                 print '[*] Serving %s their inbox' % client_addr[0]
 
-            elif 'GET /info HTTP/1.1' in query:
+            elif 'GET /info HTTP/1.1' in request.split('\r\n'):
                 print '[*] Serving %s information' % client_addr[0]
                 html_engine.display_information(client_addr[0], user_agent)
                 client.send(open('info.html', 'rb').read())
                 os.remove('info.html')
-            if 'GET /Shares HTTP/1.1' in query and not new_client:
+            elif 'GET /Shares HTTP/1.1' in request.split('\r\n') and not new_client:
                 print '[*] Serving %s html rendering of their local share folder' % client_addr[0]
                 client.send(html_engine.render_file_structure('../SHARED/'))
-            if 'GET /Upload HTTP/1.1' in query and not new_client:
+            elif 'GET /Upload HTTP/1.1' in request.split('\r\n') and not new_client:
                 print '[*] %s is uploading a file to the server' % client_addr[0]
                 client = html_engine.display_upload_page(client)
                 time.sleep(0.1)
-
-            # LOGIN
+            elif 'GET /FAQ HTTP/1.1' in request.split('\r\n'):
+                print '[*] Serving %s the FAQ page' % client_addr[0]
+                client.send(open('assets/faq.html', 'rb').read())
+            # Login attempts TODO: Encrypt how credentials are sent over the wire
             if len(request.split('username=')) > 1:
-                d, l = create_timestamp()
-                open(log_file_name, 'a').write('[*] %s is attempting to login [%s]\n' % (client_addr[0], l))
                 uname = request.split('username=')[1].split('&')[0]
                 passwd = request.split('password=')[1].split('%')[0]
                 if uname in registered_users.keys() and registered_users[uname] == passwd:
@@ -175,11 +125,17 @@ def run(handler):
                     open(log_file_name, 'a').write('[!] %s FAILED to login as %s\n' % (client_addr[0], uname))
                     print '[*] Login failure for %s' % uname
                     client.send(open('login.html', 'rb').read())
+
             client.close()
+            # HTTP 100 Continue: The server has received the request headers,
+            # and the client should proceed to send the request body
+            #
+            # HTTP 200 OK: The request is OK (this is the standard response for successful HTTP requests)
 
     except KeyboardInterrupt:
         print '[!!] Server Killed'
         os.system('sh ../kill_listeners.sh >> /dev/null 2>&1')
+        running = False
         pass
     handler.close()
 
@@ -198,7 +154,7 @@ if __name__ == '__main__':
     os.system('$(python engine.py -l %d) & ' % runtime)
 
     # Start HTTP Server
-    # tic = time.time()
+    tic = time.time()
     # Start a listening socket on port 80
     run(create_listener())
 
