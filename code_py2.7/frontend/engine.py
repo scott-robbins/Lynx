3410824574f0b6076d474b8ac590dea370ca16d9
TylersDurden <srobbin1@ithaca.edu>
2020-02-09 20:51:20 -0500

    adding support for p2p encrypted messaging

--- code_py2.7/frontend/engine.py
+++ code_py2.7/frontend/engine.py
@@ -132,7 +132,7 @@ class QueryApi:
                 enc_data = client.recv(1500000)
                 print ' [*] Message Received!'
                 decrypted_data = utils.DecodeAES(cipher, enc_data)
-                open('messages.txt', 'a').write(decrypted_data)
+                open('messages.txt', 'wb').write(decrypted_data)
         except IndexError:
             print '[!!] Message Handler Error'
         return client
