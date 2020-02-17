debbd7daf9f5b1d1b96adf0f87a0a4df2822041d
TylersDurden <srobbin1@ithaca.edu>
2020-02-16 21:58:43 -0500

    ugh I keep breaking this shit when I try to clean it up

--- code_py2.7/frontend/engine.py
+++ code_py2.7/frontend/engine.py
@@ -102,6 +102,7 @@ def fragmented(fname, frag_size):
         print "[!] Cannot find %s" % fname
     else:
         n_files = os.path.getsize(fname)/frag_size
+        print '[*] Fragmenting %s into %d files' % (fname, n_files)
         os.system('mkdir chunks/')
         raw_data = open(fname,'rb').read()
         block_ind = 1
@@ -150,7 +151,7 @@ class QueryApi:
                 open('shared_data.txt', 'wb').write(encrypted_content)
                 fragments = fragmented('shared_data.txt', 800)
                 n_frags = len(fragments['frags'])
-                # print '[*] Fragmenting shared file data into %d packets' % n_frags
+                print '[*] Fragmenting shared file data into %d packets' % n_frags
                 os.remove('shared_data.txt')
                 os.system('rm -rf chunks/')
         except IndexError:
@@ -289,22 +290,21 @@ def listen_alt_channel(timeout):
                 if decrypted_query == 'show_peers':
                     # Display peer names command
                     client = QueryApi.show_peers(client, clients, raw_data, decrypted_query)
+                    continue
 
-                elif ('GET' or 'PUT') in decrypted_query.split('_'):
-                    # Upload file
-                    client = QueryApi.file_upload(client, client_addr[0], raw_data, decrypted_query)
-                elif decrypted_query == 'send_message':
+                if decrypted_query == 'send_message':
                     # check for encrypted p2p messages
                     client = QueryApi.message_handler(client, clients, raw_data, decrypted_query)
                     continue
-                elif 'fragments' in decrypted_query.split(':'):
+                if 'fragments' in decrypted_query.split(':'):
                     N = decrypted_query.split(':')[1].split(' = ')[0]
                     name_out = decrypted_query.split(' = ')[1]
                     print '[*] %s is requesting fragmented file re-assembly of %s fragments' %\
                           (client_addr[0], N)
                     defragment(int(N), name_out)
 
-
+                # Upload file
+                client = QueryApi.file_upload(client, client_addr[0], raw_data, decrypted_query)
 
             # Check for add user command
             check_for_add_user_cmd(raw_data,client_addr, existing_users)
