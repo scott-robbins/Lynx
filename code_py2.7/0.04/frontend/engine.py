f6ddbed6db0fa2b825ab6eeb7bf6c3e3cf33958f
TylersDurden <srobbin1@ithaca.edu>
2020-02-17 10:30:10 -0500

    hmm. Well I figured out the regression Ive been continually introduced.

--- code_py2.7/0.04/frontend/engine.py
+++ code_py2.7/0.04/frontend/engine.py
@@ -282,7 +282,8 @@ def listen_alt_channel(timeout):
             # Encrypted API Queries
             if len(raw_data.split(' ???? ')) >= 2:
                 if raw_data.split(' ???? ')[0] not in clients.keys():
-                    print '[*] Do not recognize key: %s'%  raw_data.split(' ???? ')[0]
+                    print '[*] Do not recognize key: %s from %s '% \
+                          (raw_data.split(' ???? ')[0], client_addr[0])
                 cipher = AES.new(base64.b64decode(raw_data.split(' ???? ')[0]))
                 decrypted_query = utils.DecodeAES(cipher, raw_data.split(' ???? ')[1])
 
