7e3e24edcb93653e7a5d6ca5c7289d1f359c924e
TylersDurden <srobbin1@ithaca.edu>
2020-02-19 20:17:07 -0500

    touchups

--- code_py2.7/0.04/frontend/engine.py
+++ code_py2.7/0.04/frontend/engine.py
@@ -294,6 +294,11 @@ def listen_alt_channel(timeout):
                 cipher = AES.new(base64.b64decode(raw_data.split(' ???? ')[0]))
                 decrypted_query = utils.DecodeAES(cipher, raw_data.split(' ???? ')[1])
 
+                if len(decrypted_query.split('_')) >= 2:
+                    # Upload file
+                    print '[*] %s in uploading a file' % client_addr[0]
+                    client = QueryApi.file_upload(client, client_addr[0], raw_data, decrypted_query)
+
                 if decrypted_query == 'cam_ready':
                     print '[*] CamReady Message Received'
 
@@ -303,11 +308,8 @@ def listen_alt_channel(timeout):
                 # Check for show shares command
                 client = QueryApi.show_shared_files(client, raw_data, decrypted_query)
 
-                if len(decrypted_query.split('_')) >= 2:
-                    # Upload file
-                    client = QueryApi.file_upload(client, client_addr[0], raw_data, decrypted_query)
 
-                elif decrypted_query == 'send_message':
+                if decrypted_query == 'send_message':
                     # check for encrypted p2p messages
                     client = QueryApi.message_handler(client, clients, raw_data, decrypted_query)
 
