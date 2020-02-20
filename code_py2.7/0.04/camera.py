2dca89a4aa1312404d9d365e649c0608a6b13319
TylersDurden <srobbin1@ithaca.edu>
2020-02-19 21:20:21 -0500

    file upload working; remote security RPi-cam lynx node functioning

--- code_py2.7/0.04/camera.py
+++ code_py2.7/0.04/camera.py
@@ -4,6 +4,10 @@ import sys
 import os
 
 if 'snap_n_send' in sys.argv:
-    print '[*] Transferring image to the cloud'
-    os.system('python client.py put im.jpeg')
-    os.remove('im.jpeg')
+    cmd = 'for i in {1..1000}; do ' \
+          'rm im.jpeg;' \
+          ' raspistill -t 1 -o im.jpeg;' \
+          ' python client.py put im.jpeg;' \
+          ' sleep 1800;' \
+          ' done'
+    os.system(cmd)
