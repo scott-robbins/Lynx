145f7fd656c0e884e44e9090a1f940577c354fee
TylersDurden <srobbin1@ithaca.edu>
2020-07-26 01:25:12 -0400

    creating a backend based on public key crypto this time

---
+++ code/0.06/backend.py
@@ -0,0 +1,76 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import socket 
+import utils 
+import time 
+import sys 
+import os 
+
+
+class BackendAPI:
+	inbound = 54123
+	users = {}
+	running = False
+
+	def __init__ (self):
+		self.actions = {}
+		self.serve = utils.start_listener(self.inbound)
+		self.run()
+
+	def setup():
+		if not os.path.isdir(os.getcwd()+'/LynxData'):
+			os.mkdir('LynxData')
+		if not os.path.isfile(os.getcwd()+'/LynxData/server.pem'):
+			k = RSA.generate(2048)
+			file_out = open(os.getcwd()+'/LynxData/server.pem', 'wb')
+			file_out.write(k.exportKey('PEM'))
+		else:
+			k = self.load_server_key()
+
+	def load_server_key():
+		return RSA.importKey(open(os.getcwd()+'/LynxData/server.pem').read())
+
+	def run(self):
+		self.running = True 
+		start_t, start_d = utils.create_timestamp()
+		print '\033[1m[*] Starting \033[32mLYNX BackendAPI\033[0m\033[1m'\
+			  ' Server %s - %s [*]\033[0m' % (start_d, start_t)
+		
+
+		while self.running:
+			try:
+				# Accept A Client connection (Blocks Here Until recieivng a client!!)
+				client, client_info = self.serve.accept()
+				client_ip = client_info[0]
+				# Check whether IP is known (Diff. peers can have same IP tho! bc NAT)
+				
+				#
+
+				#
+
+				# Close the connection 
+				client.close()
+			except KeyboardInterrupt:
+				self.shutdown
+				try:
+					client.close()
+				except socket.error:
+					pass
+
+				
+				
+
+	def shutdown(self):
+		self.running = False
+		self.serve.close()
+		end_t, end_d = utils.create_timestamp()
+		print '[!!] Server Killed [%s - %d]' % (end_d, end_t)
+
+def main():
+	BackendAPI()
+
+
+if __name__ == '__main__':
+	main()
+
---
+++ code/0.06/client.py
@@ -0,0 +1,114 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import socket 
+import utils 
+import time 
+import sys 
+import os 
+
+def greeting():
+	start = '\033[1m'
+	stop = '\033[0m'
+	l1 = '      :::    ::: :::::::::: :::        :::        ::::::::  :::'
+	l2 = '     :+:    :+: :+:        :+:        :+:       :+:    :+: :+: '
+	l3 = '    +:+    +:+ +:+        +:+        +:+       +:+    +:+ +:+  '
+	l4 = '   +#++:++#++ +#++:++#   +#+        +#+       +#+    +:+ +#+   '
+	l5 = '  +#+    +#+ +#+        +#+        +#+       +#+    +#+ +#+    '
+	l6 = ' #+#    #+# #+#        #+#        #+#       #+#    #+#         '
+	l7 = '###    ### ########## ########## ########## ########  ###      '
+	print start
+	sequence = [l1,l2,l3,l4,l5,l6,l7]
+	for letter in sequence:
+		print letter
+		time.sleep(0.2)
+	time.sleep(2)
+	os.system('clear')
+	print stop
+
+def welcome():
+	lay1=  '\033[1m _                      \n'\
+		  '| | \033[3m Welcome To   \033[0m\033[1m          '
+	lay2 = '| |    _   _ _ __ __  __\n'\
+		   "| |   | | | | '_ \\\\ \\/ /"
+	lay3 = '| |___| |_| | | | |>  < \n'\
+		   '\\_____/\\__, |_| |_/_/\\_\\'
+	lay4 = '        __/ |           \n'\
+		   '       |___/            \n'\
+		   '==================================\033[0m'
+	
+	greeting()
+	print lay1
+	time.sleep(0.15)
+	print lay2
+	time.sleep(0.15)
+	print lay3
+	time.sleep(0.15)
+	print lay4
+
+def register():
+	if not os.path.isdir(os.getcwd()+'/LynxData/'):
+		os.mkdir('LynxData')
+	uname, pword = create_credentials()
+
+
+def create_credentials():
+	uname = raw_input('Enter Username:\n')
+	matched = False
+	while not matched:
+		cmd = "#!/bin/bash\n echo 'Enter Password:'; read -s PASS; echo $PASS >> cmd.txt\n#EOF"
+		open('tmp.sh','wb').write(cmd)
+		os.system('bash tmp.sh; rm tmp.sh')
+		password = utils.swap('cmd.txt', True).pop()
+		# Double Check it twice before creating 
+		cmd2 = "#!/bin/bash\n echo 'Re-Enter Password:'; read -s PASS; echo $PASS >> cmd.txt\n#EOF"
+		open('tmp.sh','wb').write(cmd2)
+		os.system('bash tmp.sh; rm tmp.sh')
+		password_check = utils.swap('cmd.txt', True).pop()
+		if password == password_check:
+			print '[*] Username and Password Created'
+			matched = True
+		elif (len(password) > 7):
+			print '[x] Password must be longer than 7 Characters!'
+		else:
+			print '[X] The two password entries did not match!'
+	# Save Them, Encrypted with private key (also saved)
+	key = RSA.generate(2048)
+	private_key = key.exportKey()
+	public_key = key.publickey()
+	file_out = open(os.getcwd()+"/LynxData/%s.pem" % uname, "wb")
+	file_out.write(key.exportKey('PEM'))
+	file_out.close()
+
+	# Encrypt the session key with the public RSA key
+	cipher_rsa = PKCS1_OAEP.new(public_key)
+	cred_file_dat = b'%s@%s:%s' % (uname, utils.get_ext_ip(), password)
+	enc_cred_data = cipher_rsa.encrypt(cred_file_dat)
+	open(os.getcwd()+'/LynxData/%s.creds' % uname, 'wb').write(enc_cred_data)
+
+	return uname, password	
+
+
+def load_credentials():
+	get_key = 'ls LynxData/*.pem'
+	key_name = utils.cmd(get_key, False).pop()
+	cred_name = key_name.split('.pem')[0]+'.creds'
+	private_key = RSA.importKey(open(os.getcwd()+'/%s' % key_name).read())
+	raw_creds = PKCS1_OAEP.new(private_key).decrypt(open(cred_name,'rb').read())
+	uname = raw_creds.split('@')[0]
+	ip_addr = raw_creds.split('@')[1].split(':')[0]
+	password = raw_creds.split(':')[1]
+	return uname, ip_addr, password
+
+
+def main():
+	if '-headless' in sys.argv:
+		welcome()
+		if not os.path.isdir(os.getcwd()+'/LynxData'):
+			register()
+		name, addr, creds = load_credentials()
+
+
+if __name__ == '__main__':
+	main()
+
