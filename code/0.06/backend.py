70e22748f62d40731088b0341af0d6e7cb499451
TylersDurden <srobbin1@ithaca.edu>
2020-07-28 18:18:57 -0400

    developing new backend

--- code/0.06/backend.py
+++ code/0.06/backend.py
@@ -155,7 +155,7 @@ class BackendAPI:
 				# This will not work alone because you might send more than one message a second!!
 				quick_hash = SHA256.new()
 				quick_hash.update(msg_dat)
-				msg_id = ''.join(quick_hash.digest()[0:16])
+				msg_id = ''.join(base64.b64encode(quick_hash.digest()[0:16]))
 				name = '%sFOR%sAT%s_%s.msg' % (name,recvr,when, msg_id)
 				# Write the message to disk
 				open(os.getcwd()+'/LynxData/messaging/%s/%s' % (recvr, name), 'wb').write(msg_dat)
@@ -166,6 +166,7 @@ class BackendAPI:
 				c.send('0')		
 		except IndexError:
 			print '[!!] Error receivng message'
+			print name
 			print req
 		return c
 
